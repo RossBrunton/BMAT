3114bd62f017445f295f71a28f2c77a2d1343ba8
Ross Brunton <bruntonross@gmail.com>
2015-02-26 15:00:46 +0000

    Removed some unused files

--- bookmarks/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from tags.models import Tag, colours_enum
-
-class AddTagForm(forms.ModelForm):
-    class Meta:
-        model = Tag
-        fields = ["colour", "name"]
-    
